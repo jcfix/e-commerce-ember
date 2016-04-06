fd4bc5cda50ae291ec056e7e6a062110dc5fe120
Jessica Fix and Ryan Brown <jcfix.pdx@gmail.com>
2016-04-06 16:24:50 -0700

    Add tour-detail component and move tour info to this template

--- app/components/tour-detail/component.js
+++ app/components/tour-detail/component.js
@@ -2,14 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
-
-  model() {
-    return this.store.findAll('tour');
-  },
-  
   actions: {
-    addToCart(tour) {
-      this.get('shoppingCart').add(tour);
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
     }
   }
 });
--- app/tour/route.js
+++ app/tour/route.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  shoppingCart: Ember.inject.service(),
-
   model(params) {
     return this.store.findRecord('tour', params.tour_id);
   },
-  actions: {
-    addToCart(tour) {
-      this.get('shoppingCart').add(tour);
-    }
-  }
 });
