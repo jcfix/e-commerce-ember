a6adf3c120579b42b0821ba6d010677cea6b2d06
Jessica Fix and Ryan Brown <jcfix.pdx@gmail.com>
2016-04-06 16:39:11 -0700

    End of first day commit

--- app/components/trip-tile/component.js
+++ app/components/trip-tile/component.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  shoppingCart: Ember.inject.service(),
+  shoppingCart: Ember.inject.service()
 
-  model() {
-    return this.store.findAll('tour');
-  },
-  
-  actions: {
-    addToCart(tour) {
-      this.get('shoppingCart').add(tour);
-    }
-  }
+
 });
