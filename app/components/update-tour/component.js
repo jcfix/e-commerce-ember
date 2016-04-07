c5cf93ec63ff459c4278f18d0b04370385eec82b
Jessica Fix and Ryan Brown <browneryan87@gmail.com>
2016-04-07 15:22:18 -0700

    Show previous data on updateTour function

--- app/components/update-tour/component.js
+++ app/components/update-tour/component.js
@@ -8,12 +8,12 @@ export default Ember.Component.extend({
     },
     update(tour) {
       var params = {
-        name: this.get('name') ? this.get('name') : "" ,
-        price: this.get('price') ? this.get('price') : "",
-        tourLength: this.get('tourLength') ? this.get('tourLength') : "",
-        description: this.get('description') ? this.get('description') : "",
-        image: this.get('image') ? this.get('image') : "",
-        itinerary: this.get('itinerary') ? this.get('itinerary') : ""
+        name: this.get('tour.name') ? this.get('tour.name') : "" ,
+        price: this.get('tour.price') ? this.get('tour.price') : "",
+        tourLength: this.get('tour.tourLength') ? this.get('tour.tourLength') : "",
+        description: this.get('tour.description') ? this.get('tour.description') : "",
+        image: this.get('tour.image') ? this.get('tour.image') : "",
+        itinerary: this.get('tour.itinerary') ? this.get('tour.itinerary') : ""
       };
       this.set('editTour', false);
       this.sendAction('update', tour, params);
