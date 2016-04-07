05a0292b5d5037e1e04620130b0bf77f62b49977
Jessica Fix and Ryan Brown <browneryan87@gmail.com>
2016-04-07 09:33:16 -0700

    Fix shopping-cart bug by removing <a></a> and replacing with {{link-to}} in nav

--- app/cart/route.js
+++ app/cart/route.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
-
+//Need to call on all model 'tours' in this route in order to display them in display-cart template?
 export default Ember.Route.extend({
+  // model() {
+  //   return this.store.findAll('tour');
+  // }
+  //end
 });
